ec12e947d9c746f1227bfa875d6114e03ca04cff
yuusaif <alvi676@gmail.com>
2024-12-30 23:24:28 +0600

    updated code.cpp and added code.py

--- code.cpp
+++ code.cpp
@@ -1,1 +1,15 @@
-cout << "Hello world";
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+using namespace std;
+// getline(cin>>ws,s);
+const int MOD = 1e9 + 7;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
